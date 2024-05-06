76c4cdbb9de4539f88d47c20c2f0242be3021b0a
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2024-05-06 17:32:24 +0200

    up

--- MOM1.jl
+++ MOM1.jl
@@ -56,6 +56,8 @@ bootstrap_IC(data,0.025,1000)
 
 
 
+using Distributions
+using Random
 
 
 function median_of_means(seq, n)
@@ -68,20 +70,67 @@ function median_of_means(seq, n)
     return median(moyennes)
 end
 
-median_of_means(data,5)
+X=[2,5,8,10,53,12,14,35,20,17,15,6,4,9,11,19,13,14,48,8]
+Y=[2,5,8,10,12,14,20,17,15,6,4,9,11,19,13,14,8]
+mean(Y)
 
+median(X)
+mean(X)
+median_of_means(X, 5)
 
+function median_of_mean_opt(X,alpha=0.025)
+    n=lenght(X)
+    k=abs(trunc(Int,8*log(1/alpha)))
+    m=div(n,k)
+    indic=repeat(1:k,inner=m)
+    indic=shuffle(indic)
+    moyennes=[mean(X[indic.==block]) for block in 1:k]
+    return median(moyennes) 
+end
+data1 = rand(Exponential(2), 10000)
+median_of_mean_opt(data1,0.025)
 
-
-function median_of_means_opt(seq, alpha=0.025)
-    n = length(seq)
-    k = abs(trunc(Int, 8 * log(1 / alpha)))
-    m = div(n, k)
-    means = [mean(seq[((i-1)*m+1):min(i*m, n)]) for i in 1:k]
-    return median(means)
+using Random
+function median_of_mean_opt(X,alpha=0.025)
+    n=length(X)
+    k=abs(trunc(Int,8*log(1/alpha)))
+    m=div(n,k)
+    indic=repeat(1:k,inner=m)
+    indic=shuffle(indic)
+    moyennes=[mean(X[indic.==block]) for block in 1:k]
+    return median(moyennes) 
 end
 
-median_of_means_opt(data,0.99)
+
+data1 = rand(Exponential(2), 10000)
+median_of_mean_opt(data1,0.025)
+
+using Random
+
+function median_of_mean_opt(X, alpha=0.025)
+    n = length(X)
+    k = abs(trunc(Int, 8 * log(1 / alpha)))
+    m = div(n, k)
+    indic = repeat(1:k, inner=m)
+    shuffle!(indic)
+    moyennes = [mean(X[indic .== block]) for block in 1:k]
+    return median(moyennes) 
+end
+
+data1 = rand(Exponential(2), 10000)
+println(median_of_mean_opt(data1, 0.025))
+
+function median_of_means_opt
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -111,3 +160,27 @@ alphas = collect(0.001:0.001:0.999)  # Choisir une plage d'alpha
 estimates = [median_of_means_opt(data, alpha) for alpha in alphas]
 
 plot(alphas, estimates, xlabel="Alpha", ylabel="Estimateur", label="", title="Estimateur en fonction de Alpha")
+
+
+function median_of_means_opt2(X, alpha=0.025)
+    n = length(X)
+    k = trunc(Int, 8*log(1/alpha))
+    m = n ÷ k
+    indic = repeat(1:k, inner = m)
+    indic .= shuffle(indic)  # Utilisation de shuffle pour mélanger les indices
+    moyennes = [mean(X[indic .== block]) for block in 1:k]
+    return median(moyennes)
+end
+
+median_of_means_opt2(data, 0.025)
+
+#Fonction sans le sample
+
+function median_of_means_opt(seq, alpha=0.025)
+    n = length(seq)
+    k = abs(trunc(Int, 8 * log(1 / alpha)))
+    m = div(n, k)
+    means = [mean(seq[((i-1)*m+1):min(i*m, n)]) for i in 1:k]
+    return median(means)
+end
+
